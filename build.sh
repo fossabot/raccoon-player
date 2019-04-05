3044f36f18fd3cca59d753e2d74941b485d52173
tephro <tephro@icloud.com>
2019-04-05 15:52:03 +0300

    build.s

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-go build -o /usr/local/bin/raccoon-player ${scriptDir}/main.go
+go build -o /usr/local/bin/raccoon-player -mod=vendor ${scriptDir}/main.go
